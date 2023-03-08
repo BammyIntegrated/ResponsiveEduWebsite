85b6d9fe3608b49f5662bf08dd938e270cfe357d
Michael Bamidele Olurotimi <bammydhele@gmail.com>
2023-03-08 22:20:32 +0100

    Complete Responsive Edu Web Home Page

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ window.addEventListener('scroll', () => {
 })
 
 
-// show faqs answers
+// show/hide faqs answers
 
 const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
-    faq.classList.toggle('open')
+faqs.forEach(faq =>{
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+        
+        //Change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        } else {
+             icon.className = "uil uil-plus";
+        }
 
-    // change icon
-
-    const icon = faq.querySelector('.faq__icon i');
-    if(icon.className = 'uil uil-plus'){
-        icon.className = 'uil uil-minus'
-    }
-
+    })
 })
